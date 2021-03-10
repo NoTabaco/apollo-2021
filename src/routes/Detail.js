3c8ea8f1305d6f80e166edb057601b6885344380
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-03-10 22:54:48 +0900

    Updated Getting Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,25 @@
-const Detail = () => <span>Detail</span>;
+import { useQuery, gql } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      summary
+    }
+  }
+`;
+
+const Detail = () => {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
+  console.log(loading, data);
+
+  return !loading && <span>{data.movie.title}</span>;
+};
 
 export default Detail;
